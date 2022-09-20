45e6db2b73db69b3e557634546e3e95d6cf83aeb
MaxMessWithTech <max@benmiller.com>
2022-09-20 10:52:06 -0500

    Fixed grid

--- frontend/src/components/Canvas.js
+++ frontend/src/components/Canvas.js
@@ -154,69 +154,101 @@ const Canvas = props => {
             }
         }
 
-        const drawGridLines = (startX, endX, startY, endY) => {
+        const drawVerticalGrid = (startX, endX, major) => {
             for (var i = 1; i < STEPS_TO_5_MAJOR; i++) {
                 const x = ((endX - startX) / STEPS_TO_5_MAJOR) * i + startX;
 
-                drawVerticalGirdLine(x, GRID_MAJOR_DIVISION_COLOR, 1);
-                
-                // Draw all the minor division grid lines between the major divisions
-                for (var ii = 1; ii < STEPS_TO_5_MINOR; ii++) {
-                    const x2 = ((endX - startX) / STEPS_TO_5_MINOR) * ii + startX;
+                if (major) {
+                    drawVerticalGirdLine(x, GRID_MAJOR_DIVISION_COLOR, 1);
+                } else {
+                     // Draw all the minor division grid lines between the major divisions
+                    for (var ii = 1; ii < STEPS_TO_5_MINOR; ii++) {
+                        const x2 = ((endX - startX) / STEPS_TO_5_MINOR) * ii + startX;
 
-                    if (x2 !== x) {
-                        drawVerticalGirdLine(x2, GRID_MINOR_DIVISION_COLOR, 1);
+                        if (x2 !== x) {
+                            drawVerticalGirdLine(x2, GRID_MINOR_DIVISION_COLOR, 1);
+                        }
+                    
                     }
-                
                 }
+            
             }
             
         }
 
-        const drawHorizontalGrid = (hashRatio, color) => {
-            // const startY = (canvas.height * FRONT_HASH_RATIO);
-            //const endY = (canvas.height * BACK_HASH_RATIO);
-
-            // const hashes = DISTANCE_BETWEEN_HASHES_IN_YDS / 5 * STEPS_TO_5_MAJOR;
-
-            // const startY = 0;
-            // const endY = (canvas.height * FRONT_HASH_RATIO);;
-
-            // const hashes = DISTANCE_BETWEEN_HASHES_IN_YDS / 5 * STEPS_TO_5_MAJOR;
-
+        const drawHorizontalGrid = (hashRatio, major) => {
             const hashes = 28;
+
+            const majorHashes = 8;
             const hashLocation = hashRatio * canvas.height;
             // height(px) -> ?height/1" 
             // 22.5" = 1 step
             // 90" = 4 steps
             // 1920" = 
             const oneStep = canvas.height / 1920 * 22.5;
-            const hashDistance = oneStep * (28 / STEPS_TO_5_MAJOR);
+            const hashDistance = oneStep * 16;
             const startY = hashLocation - hashDistance;
             const endY = hashLocation + hashDistance;
             // console.log(hashes);
+            
+            for (var i = 1; i < majorHashes + 1; i++) {
+                const y = ((endY - startY) / (majorHashes + 1)) * i + startY;
+                const nextY = ((endY - startY) / (majorHashes + 1)) * (i + 1) + startY;
 
-            for (var i = 0; i < hashes / STEPS_TO_5_MAJOR; i++) {
-                const y = ((endY - startY) / (hashes / STEPS_TO_5_MAJOR)) * i + startY;
-                const nextY = ((endY - startY) / (hashes / STEPS_TO_5_MAJOR)) * (i + 1) + startY;
+                if (y > canvas.height) { break; }
 
-                drawHorizontalGirdLine(y, color, 1);
-                
-                
+                if (major) {
+                    drawHorizontalGirdLine(y, GRID_MAJOR_DIVISION_COLOR, 1);
+                }
                 // Draw all the minor division grid lines between the major divisions
-                for (var ii = 1; ii <= STEPS_TO_5_MINOR / STEPS_TO_5_MAJOR; ii++) {
-                    const y2 = ((y - nextY) /  (STEPS_TO_5_MINOR / STEPS_TO_5_MAJOR)) * ii + y;
-
-                    if (y2 !== y) {
-                        drawHorizontalGirdLine(y2, GRID_MINOR_DIVISION_COLOR, 1);
+                else if (nextY <= endY) {
+                    for (var ii = 1; ii < (STEPS_TO_5_MINOR / STEPS_TO_5_MAJOR); ii++) {
+                        const y2 = ((nextY - y) /  (STEPS_TO_5_MINOR / STEPS_TO_5_MAJOR)) * ii + y;
+    
+                        if (y2 !== y && y2 !== nextY) {
+                            drawHorizontalGirdLine(y2, GRID_MINOR_DIVISION_COLOR, 1);
+                        }
+                    
                     }
-                
                 }
                 
             }
         }
 
+        const drawGridLines = () => {
+            drawHorizontalGrid(0, false);
+            drawHorizontalGrid(FRONT_HASH_RATIO, false);
+            drawHorizontalGrid(BACK_HASH_RATIO, false);
+            drawHorizontalGrid(1, false);
+
+            for (var x = 0; x < 21; x++) {
+                var val = x * (canvas.width / 20);
+                var nextVal = (x + 1) * (canvas.width / 20);
+
+                if (nextVal <= canvas.width) {
+                    drawVerticalGrid(val, nextVal, false);
+                }
+            }
+
+            drawHorizontalGrid(0, true);
+            drawHorizontalGrid(FRONT_HASH_RATIO, true);
+            drawHorizontalGrid(BACK_HASH_RATIO, true);
+            drawHorizontalGrid(1, true);
+
+            for (var x = 0; x < 21; x++) {
+                var val = x * (canvas.width / 20);
+                var nextVal = (x + 1) * (canvas.width / 20);
+
+                if (nextVal <= canvas.width) {
+                    drawVerticalGrid(val, nextVal, true);
+                }
+            }
+        }
+
         const drawGrid = () => {
+            // Draw Grid Lines
+            drawGridLines();
+
             // This draws the 5 yard lines up through the 50, from the left
             for (var x = 0; x < 21; x++) {
                 var val = x * (canvas.width / 20);
@@ -228,8 +260,6 @@ const Canvas = props => {
                 if (nextVal <= canvas.width) {
                     drawHash(val, nextVal, canvas.height * FRONT_HASH_RATIO);
                     drawHash(val, nextVal, canvas.height * BACK_HASH_RATIO);
-
-                    drawGridLines(val, nextVal, 0, canvas.height);
                 }
 
                 // Draw little lines on the hash marks | FRONT HASH
@@ -248,7 +278,7 @@ const Canvas = props => {
                     canvas.height * BACK_HASH_RATIO,
                     "black", 1
                 );
-
+                
                 context.beginPath();
                 context.font = canvas.height * 0.05 + 'px serif';
                 context.textBaseline = "middle";
@@ -262,19 +292,6 @@ const Canvas = props => {
             
                 context.closePath();
             }
-
-            // drawHorizontalGrid(0, (canvas.height * FRONT_HASH_RATIO));
-            drawHorizontalGrid(0, "rgb(0, 255, 0)");
-            drawHorizontalGrid(FRONT_HASH_RATIO, "rgb(0, 0, 255)");
-            drawHorizontalGrid(BACK_HASH_RATIO, "rgb(255, 0, 0)");
-            drawHorizontalGrid(1, "rgb(0, 255, 0)");
-            // drawHorizontalGrid((canvas.height * BACK_HASH_RATIO), canvas.height);
-
-            // Draw the front HS Hash
-            // drawHorizontalGirdLine(canvas.height * (1 / 3), "black", 1);
-            
-            // Draw the back HS Hash
-            // drawHorizontalGirdLine(canvas.height * (2 / 3), "black", 1);
         }
 
         const clear = () => {
@@ -294,8 +311,8 @@ const Canvas = props => {
                 canvasRef.current.style.width = "100%";
                 canvasRef.current.style.height = "100%";
 
-                const heightRatio = canvas.offsetWidth * 4 / 9;
-                const widthRatio = canvas.offsetHeight * 9 / 4;
+                const heightRatio = canvas.offsetWidth * 3 / 5;
+                const widthRatio = canvas.offsetHeight * 5 / 3;
 
                 if (heightRatio > canvas.offsetHeight && widthRatio ) {
                     canvas.width  = widthRatio;
