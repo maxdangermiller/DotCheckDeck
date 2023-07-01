7c7c3bce33d584ebe9883755576e10b11c3d4a17
Max Miller <maxmiller@maxs-imac.lan>
2023-07-01 18:10:32 -0500

    Formated basic a bit

--- frontend/src/components/BasicViewer.js
+++ frontend/src/components/BasicViewer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import getApi from './getApi';
 import axios from "axios";
+import { AutoTextSize } from 'auto-text-size'
 
 const WINDOW_LOCATION = getApi();
 
@@ -40,15 +41,15 @@ const BasicViewer = (props) => {
     const getDotText1 = (dot) => {
         if (dot.steps !== 0) {
             return (
-                <>
+                <strong>
                 {dot.steps} steps {dot.direction} {dot.line}yd line, on side {dot.side}
-                </>
+                </strong>
             );
         }
         return (
-            <>
+            <strong>
             On {dot.line}yd line, on side {dot.side}
-            </>
+            </strong>
         );
     }
 
@@ -56,29 +57,33 @@ const BasicViewer = (props) => {
         if (dot.fb_steps !== 0) {
             let fbDirection = dot.fb_direction === "Front" ? "in front of" : dot.fb_direction;
             return (
-                <>
+                <strong>
                 {dot.fb_steps} steps {fbDirection} {dot.use_hash}
-                </>
+                </strong>
             );
         }
         return (
-            <>
+            <strong>
             On {dot.use_hash}
-            </>
+            </strong>
         );
     }
 
 
     return (
         <div className="flex-column justify-content-center d-flex align-items-center ViewerFullScreen">
-            <div className="overflow-auto" style={{height: '80vh'}}>
+            <div className="overflow-auto" style={{height: '80vh', width:'min(100%, 800px'}}>
             <ul className="list-group">
             {
                 data.dots.map((dotData, index) =>
-                    <li className="list-group-item flex-row justify-content-between d-flex align-items-center" key={index} style={{width:'100%'}}>
-                        <div className='flex-column justify-content-center d-flex align-items-center' style={{width:'30%'}}>
-                            <h1>{dotData.set_numb}</h1>
-                            <p><strong>{dotData.set_name}</strong></p>
+                    <li className="list-group-item flex-row justify-content-between d-flex align-items-center" key={index} style={{width:'100%', height:'14vh'}}>
+                        <div className='flex-column justify-content-center d-flex align-items-center' style={{width:'30%', height:'100%'}}>
+                            <div style={{maxWidth:'100%', height:'60%'}} className='flex-column justify-content-center d-flex align-items-center'>
+                                <AutoTextSize mode='box'>{dotData.set_numb}</AutoTextSize>
+                            </div>
+                            <div style={{maxWidth:'100%', height:'40%'}} className='flex-column justify-content-center d-flex align-items-center'>
+                                <AutoTextSize mode='box'><strong>{dotData.set_name}</strong></AutoTextSize>
+                            </div>
                         </div>
                         <div className='flex-column justify-content-center d-flex align-items-start' style={{width:'70%'}}>
                             <div className='mb-2'>{getDotText1(dotData.dot)}</div>
