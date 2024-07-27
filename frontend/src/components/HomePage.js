907930c06ecb7e3273364565894a70c9e58cd3f6
Max Miller <max@benmiller.com>
2024-07-27 16:58:31 -0500

    Added processer for displaying credits on home page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -38,35 +38,38 @@ const HomePage = (props) => {
         }
     }
 
-    const getBtnDivStyle = () => {
+    const getBtnBottom = () => {
         const BUTTON_LOCATION = 300;
         const MIN_FROM_BOT = 150;
         const ABSOLUTE_MIN_FROM_BOT = 40;
 
+        let height = imageHeight;
+        // let width = imageWidth;
+        
+        let parentHeight = getDivHeight();
+
+        let scale = height / 1000;
+        let overlap = (height - parentHeight) / 2;
+
+        let y = (BUTTON_LOCATION * scale) - overlap;
+
+        if (y < MIN_FROM_BOT && parentHeight > MIN_FROM_BOT * 3) {
+            y = MIN_FROM_BOT;
+        }
+        else if (y < ABSOLUTE_MIN_FROM_BOT) {
+            y = ABSOLUTE_MIN_FROM_BOT;
+        }
+
+        console.log("Getting Btn Pos: image height = ", height, ", scale=", scale, ", overlap=", overlap, " -> y=", y);
+
+        return y;
+    }
+
+    const getBtnDivStyle = () => {
         try { 
-            let height = imageHeight;
-            let width = imageWidth;
-            
-            let parentHeight = getDivHeight();
+            let bottomY = getBtnBottom();
 
-            let scale = height / 1000;
-            let overlap = (height - parentHeight) / 2;
-
-            let y = (BUTTON_LOCATION * scale) - overlap;
-
-            // console.log(height, scale, overlap, BUTTON_LOCATION * scale - overlap)
-            console.log("Updating Btn Pos: image height = ", height, ", scale=", scale, ", overlap=", overlap, " -> y=", y);
-            
-            // setButtonY(BUTTON_LOCATION * scale - overlap);
-
-            if (y < MIN_FROM_BOT && parentHeight > MIN_FROM_BOT * 3) {
-                y = MIN_FROM_BOT;
-            }
-            else if (y < ABSOLUTE_MIN_FROM_BOT) {
-                y = ABSOLUTE_MIN_FROM_BOT;
-            }
-
-            return {width: getDivWidth(), bottom: y, left: "0px", position: "fixed"}
+            return {width: getDivWidth(), bottom: bottomY, left: "0px", position: "fixed"}
         
         } catch (error) {
             console.log(error);
@@ -89,6 +92,19 @@ const HomePage = (props) => {
         };
     }, [])
 
+    const canDisplayCredits = () => {
+        const WIDTH_OF_CREDITS = 540;
+        const HEIGHT_OF_CREDITS = 48;
+        const SPACING_FROM_BTN = 20;
+
+        const buttonBottom = getBtnBottom();
+
+        if (buttonBottom >= HEIGHT_OF_CREDITS + SPACING_FROM_BTN && getDivWidth() >= WIDTH_OF_CREDITS) {
+            return true;
+        }
+        return false;
+    }
+
     return (
         <div className="d-flex justify-content-center align-items-center flex-column" style={{width: getDivWidth(), height: getDivHeight(), backgroundColor: "#212429"}}>
             <img src={bannerLogo} alt="" width={imageWidth} height={imageHeight}/>
@@ -106,14 +122,19 @@ const HomePage = (props) => {
             </div>
             <div 
                 className="d-flex justify-content-center align-items-center flex-column" 
-                style={{width: getDivWidth(), bottom: "0px", left: "0px", position: "fixed", color: "white"}}
-            >   
-                {
-                    !isMobile ?
-                    <span>Dot Check Deck Created By Max Miller; Graphic Design by Allison Kroesch</span>
-                    : null
-                }
-                <span>© 2024, DotCheckDeck.com</span>
+                style={{width: getDivWidth(), bottom: "0px", left: "0px", position: "absolute", color: "white"}}
+            > 
+                <div 
+                    className='d-flex justify-content-center align-items-center flex-column'
+                    style={{backgroundColor: "rgba(33, 36, 41, 0.6)"}}
+                >
+                    {
+                        canDisplayCredits() ?
+                        <span>Dot Check Deck Created By Max Miller; Graphic Design by Allison Kroesch</span>
+                        : null
+                    }
+                    <span>© 2024, DotCheckDeck.com</span>
+                </div>
             </div>
         </div>
     );
